18153f699998f5a6c50ff9f78ad7baa666f3b861
sograli <lll145bb120120@qq.com>
2026-08-15 23:00:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'beisong-dungeon-v104';
+const CACHE = 'beisong-dungeon-v105';
 const ASSETS = ['./', './index.html', './chinese-content.js', './manifest.webmanifest', './app-icon.svg'];
 self.addEventListener('message', event => { if (event.data === 'SKIP_WAITING') self.skipWaiting(); });
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
